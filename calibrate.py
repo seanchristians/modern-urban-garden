5ef1b1af6194ba29936283633d8541ad42b44882
Sean Christians <seanchristians.scc@gmail.com>
2020-08-29 15:58:47 -0400

    fix: invoke generator properly

--- calibrate.py
+++ calibrate.py
@@ -3,11 +3,14 @@ import gpiozero, os, time, signal
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from devices import pump, button
 
-def calibrate():
+def calibrator():
     start = time.time()
     pump.on()
     yield
     pump.off()
-    print(time.time() - start)
+    return time.time() - start
 
-button.when_pressed = calibrate
+calibrate = calibrator()
+button.when_pressed = lambda: next(calibrate)
+
+signal.pause()
